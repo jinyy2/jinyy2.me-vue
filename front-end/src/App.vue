b0548045e5b94231da8893d175a81f927243d839
jinyy2 <jy5031le@gmail.com>
2020-11-28 18:58:40 +0900

    Merge navi & Add button

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,38 +1,53 @@
 <template>
-<v-app id="inspire">
-  <v-icon>fas fa-list</v-icon>
-    <!-- <v-system-bar app>
-      <v-spacer></v-spacer>
-<v-icon>fas fa-list</v-icon>
-    </v-system-bar> -->
-      <!-- <v-btn v- @click="Navigation" text rounded>Navigation</v-btn> -->
-      <v-btn @click="toggleTheme" text rounded>toggle Theme</v-btn>
-    <navigation />
-    
-    
+  <v-app id="inspire">
+    <!-- <v-btn @click="toggleTheme" text rounded>toggle Theme</v-btn> -->
+
+    <!-- <navigation /> -->
+    <v-navigation-drawer v-model="drawer" app>
+      <v-sheet color="grey lighten-1" class="pa-4">
+        <v-avatar class="mb-4" color="grey darken-1" size="64"></v-avatar>
+
+        <div>jy5031le@gmail.com</div>
+      </v-sheet>
+
+      <v-divider></v-divider>
+
+      <v-list>
+        <v-switch
+          class="pl-8"
+          v-model="$vuetify.theme.dark"
+          inset
+          color="indigo darken-3"
+          label="Dark Mode"
+          persistent-hint
+        ></v-switch>
+        <v-list-item v-for="[icon, text] in links" :key="icon" link>
+          <v-list-item-icon>
+            <v-icon>{{ icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title @click="move(text)">{{
+              text
+            }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
     <v-main>
-      <v-container
-        class="py-8 px-6"
-        fluid
-      >
-      <router-view></router-view>
+      <v-btn v-if="!drawer" @click="drawer = true">nav-bar</v-btn>
+      <v-container class="py-8 px-6" fluid>
+        <router-view></router-view>
         <v-row>
-          11
           <v-icon>fas fa-list</v-icon>
-          <v-col
-            v-for="card in cards"
-            :key="card"
-            cols="12"
-          >
+          <v-col v-for="card in cards" :key="card" cols="12">
             <v-card>
               <v-subheader>{{ card }}</v-subheader>
 
               <v-list two-line>
                 <template v-for="n in 6">
-                  <v-list-item
-
-                    :key="n"
-                  >
+                  <v-list-item :key="n">
                     <v-list-item-avatar color="grey darken-1">
                     </v-list-item-avatar>
 
@@ -40,7 +55,8 @@
                       <v-list-item-title>Message {{ n }}</v-list-item-title>
 
                       <v-list-item-subtitle>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil repellendus distinctio similique
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit. Nihil repellendus distinctio similique
                       </v-list-item-subtitle>
                     </v-list-item-content>
                   </v-list-item>
@@ -67,31 +83,39 @@
 </template>
 
 <script>
-import Navigation from '@/components/common/Navigation.vue'
-
+// import Navigation from "@/components/common/Navigation.vue";
 
 export default {
-  name: 'App',
+  name: "App",
 
   components: {
-    Navigation
+    // Navigation,
   },
 
   data: () => ({
-     cards: ['Today', 'Yesterday'],
-      // drawer: null,
-      // links: [
-      //   ['mdi-inbox-arrow-down', 'Home'],
-      //   ['mdi-send', 'About'],
-      //   ['mdi-delete', 'Trash'],
-      //   ['mdi-alert-octagon', 'Spam'],
-      // ],
+    cards: ["Today", "Yesterday"],
+    drawer: null,
+    theme: false,
+    links: [
+      ["mdi-inbox-arrow-down", "Home"],
+      ["mdi-send", "About"],
+      ["mdi-delete", "Trash"],
+      ["mdi-alert-octagon", "Spam"],
+    ],
   }),
-  methods:{
-
-    toggleTheme(){
-      this.$vuetify.theme.dark=!this.$vuetify.theme.dark
-    }
-  }
+  methods: {
+    move(m) {
+      // alert(m)
+      this.$router.push(`${m}`);
+    },
+    toggleTheme() {
+      this.$vuetify.theme.dark = !this.$vuetify.theme.dark;
+    },
+  },
+  watch: {
+    theme() {
+      this.toggleTheme();
+    },
+  },
 };
 </script>
