04e1348c32ce56c0b1155e1ac792a7124701c300
jinyy2 <jy5031le@gmail.com>
2020-11-28 15:45:15 +0900

    Add main-page wireframe

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,60 +1,136 @@
 <template>
-  <v-app>
-    <!-- <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
+<v-app id="inspire">
+    <!-- <v-system-bar app>
       <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
+      <v-icon>mdi-square</v-icon>
+
+      <v-icon>mdi-circle</v-icon>
+
+      <v-icon>mdi-triangle</v-icon>
+    </v-system-bar> -->
+      <v-btn @click="toggleTheme" text rounded>toggle Theme</v-btn>
+
+    <v-navigation-drawer
+      v-model="drawer"
+      permanent
+      expand-on-hover
+      app
+    >
+      <v-sheet
+        color="grey lighten-4"
+        class="pa-4"
       >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar> -->
+        <v-avatar
+          class="mb-4"
+          color="grey darken-1"
+          size="64"
+        ></v-avatar>
+
+        <div>jy5031le@gmail.com</div>
+      </v-sheet>
+
+      <v-divider></v-divider>
+
+      <v-list>
+        <v-list-item
+          v-for="[icon, text] in links"
+          :key="icon"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title @click="move(text)">{{ text }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    
     <v-main>
-      <Home/>
-    <router-view />
+      <v-container
+        class="py-8 px-6"
+        fluid
+      >
+      <router-view></router-view>
+        <v-row>
+          <v-col
+            v-for="card in cards"
+            :key="card"
+            cols="12"
+          >
+            <v-card>
+              <v-subheader>{{ card }}</v-subheader>
+
+              <v-list two-line>
+                <template v-for="n in 6">
+                  <v-list-item
+
+                    :key="n"
+                  >
+                    <v-list-item-avatar color="grey darken-1">
+                    </v-list-item-avatar>
+
+                    <v-list-item-content>
+                      <v-list-item-title>Message {{ n }}</v-list-item-title>
+
+                      <v-list-item-subtitle>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil repellendus distinctio similique
+                      </v-list-item-subtitle>
+                    </v-list-item-content>
+                  </v-list-item>
+
+                  <v-divider
+                    v-if="n !== 6"
+                    :key="`divider-${n}`"
+                    inset
+                  ></v-divider>
+                </template>
+              </v-list>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
     </v-main>
   </v-app>
+  <!-- <v-app>
+    <v-btn @click="toggleTheme" text rounded>toggle Theme</v-btn>
+    <v-main>
+    <router-view />
+    </v-main>
+  </v-app> -->
 </template>
 
 <script>
-import Home from '@/views/Home';
+// import Home from '@/views/Home';
 
 export default {
   name: 'App',
 
   components: {
-    Home,
+    // Home,
   },
 
   data: () => ({
-    //
+     cards: ['Today', 'Yesterday'],
+      drawer: null,
+      links: [
+        ['mdi-inbox-arrow-down', 'Home'],
+        ['mdi-send', 'About'],
+        ['mdi-delete', 'Trash'],
+        ['mdi-alert-octagon', 'Spam'],
+      ],
   }),
+  methods:{
+    move(m){
+      // alert(m)
+      this.$router.push(`${m}`)
+    },
+    toggleTheme(){
+      this.$vuetify.theme.dark=!this.$vuetify.theme.dark
+    }
+  }
 };
 </script>
